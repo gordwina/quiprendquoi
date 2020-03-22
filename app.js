78b8d95bab68c393839f67ef3fdd3f7ed7c98250
alice-tkt <alice@wertkt.com>
2020-03-22 17:15:22 +0100

    Servir un template pug

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
 
+
+app.set('view engine', 'pug');
+
 app.get('/', function(req, res) {
-  res.send('Message de votre choix');
+  res.send('index');
 });
+
+
 
 app.listen(process.env.PORT, () =>
   console.log(`Front app listening on port ${ process.env.PORT }!`),
 );
+

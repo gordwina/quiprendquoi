48853ed2dc2c0e324f51ae42a96ede86f375c783
alice-tkt <alice@wertkt.com>
2020-03-22 18:08:49 +0100

    récupérer les parametre du post

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
 const app = express();
 
 
 
 app.set('view engine', 'pug');
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function(req, res) {
   res.render('index',{title: "qui prend quoi ? "});
 });
 
 app.post('/party', (req, res) => {
+    console.log(req.body);
     res.send('Post that! ');
 });
 
